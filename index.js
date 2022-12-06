6cd714cab4bfb5f9ae86d7c2ca1ead7d7a123189
tonykwasi <godisgoodtome2007@gmail.com>
2022-12-06 18:43:33 +0000

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-const express = require("express")
-const app = express()
+const express = require("express");
 
-app.get('/', (req, res) => {
-    res.send('Hello There')
-})
+const app = express();
+const port = 3000;
 
+//parse JSON using express
+app.use(express.json());
+app.use(express.urlencoded ({ extended: false}));
 
-app.listen(3000, () => {
-    console.log(`Server started on port 3000`)
-}) 
+let movies = [
+  {
+    id: "1",
+    title: "inception",
+    director: "Christopher Nolan",
+    release_date: "2010-07-16"
+  },
+  {
+    id: "2",
+    title: "SinBad",
+    director: "seth Duncan",
+    release_date: "2010-03-24"
+  },
+  {
+    id: "3",
+    title: "Rambo",
+    director: "Stallonw Francis",
+    release_date: "2023-12-09"
+  }
+];
+
+// Get all movie list
+app.get("/movie", (req, res) => {
+  res.json(movies);
+});
+
+//add movie to the list
+app.post("/movie", (req, res) => {
+  const movie = req.body;
+
+  console.log(movie);
+  movies.push(movie);
+  res.send("Movie is added to the liat!");
+});
+
+//set server to listen at port
+app.listen(port, () => console.log(`Server listening at port ${port}`));
