eea778528e4210cf5eb9bcf08600ce416139afd1
tonykwasi <godisgoodtome2007@gmail.com>
2022-12-06 08:31:50 +0000

    first commit

---
+++ routes/api/users.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const uuid = require('uuid')
+let users = require('../../Users')
+
+//get all users
+router.get("/", (req, res) => {
+    res.json(users);
+});
